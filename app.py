21f6ec51dca3e9797c08699e67b3516a20b0f0cf
Freddie <137670775+freddieoman@users.noreply.github.com>
2023-06-24 21:54:41 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+import imp
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+
+root = tk.Tk()
+apps = []
+
+if os.path.isfile('save.txt'):
+    with open('save.txt', 'r') as f:
+        tempApps = f.read()
+        tempApps = tempApps.split(',')
+        apps = [x for x in tempApps if x.strip()]
+
+def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    filename = filedialog.askopenfilename(initialdir="/", title="lol choose a .exe file nerd",
+                                            filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
+
+canvas = tk.Canvas(root, height=700, width = 700, bg="#FFFFCE")
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+openFile = tk.Button(root, text="Select", padx=10, pady=5, fg="black", bg="#FFFF77", command=addApp)
+openFile.pack()
+
+runApps = tk.Button(root, text="Run", padx=10, pady=5, fg="black", bg="#FFFF77", command=runApps)
+runApps.pack()
+
+for app in apps:
+    label = tk.Label(frame, text=app)
+    label.pack()
+
+root.mainloop()
+
+with open('save.txt', 'w') as f:
+    for app in apps:
+        f.write(app + ',')
